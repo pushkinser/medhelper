c1e39dd9285ce4814cdc7036ff4310dcb739de47
pushkinser <pushkinsergey.voronezh@gmail.com>
2015-11-12 19:52:20 +0300

    Add ButtonView.java

---
+++ src/main/java/com/mycompany/medhelper/ButtonView.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.medhelper;
+
+
+ 
+import java.io.IOException;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+ 
+@ManagedBean
+public class ButtonView {
+     
+    public void buttonAction(ActionEvent actionEvent) throws IOException {
+        ExternalContext context = FacesContext.getCurrentInstance().getExternalContext(); 
+            context.redirect("index.html");
+    }
+     
+    public void addMessage(String summary) {
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, summary,  null);
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }
+}
